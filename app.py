4906ff12ea7abead984efabfc5406bc97ae33d87
abirbs23 <abir.hasan@brainstation-23.com>
2024-05-06 22:20:24 +0600

    pdf excract

--- app.py
+++ app.py
@@ -1,11 +1,23 @@
+import pdfplumber
 from flask import Flask
+from flask_cors import CORS
+from dotenv import load_dotenv
 
 app = Flask(__name__)
 
 
+def extract_text_from_pdf(pdf_path):
+    with pdfplumber.open(pdf_path) as pdf:
+        text = ""
+        for page in pdf.pages:
+            text += page.extract_text()
+    return text
+
+
 @app.route('/')
 def index():
-    return "Hello World!"
+    pdf_text = extract_text_from_pdf('pdf/Nikles Company Information Chatbot.pdf')
+    return pdf_text
 
 
 if __name__ == '__main__':
